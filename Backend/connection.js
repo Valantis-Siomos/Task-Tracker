90b0947e925e3b830974273084aaa7c1135ade69
Valantis <siopis2008@hotmail.com>
2024-01-22 08:17:54 +0200

    connect server to database

--- Backend/connection.js
+++ Backend/connection.js
@@ -8,4 +8,10 @@ const pool = mysql.createPool({
   database: process.env.DB_NAME,
 });
 
+
+
+
+
+
+
 module.exports = pool.promise();
--- Backend/server.js
+++ Backend/server.js
@@ -1,16 +1,34 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const port = 8000;
+const mysql = require("mysql2");
+require("dotenv").config();
 
 
+
+app.use(cors());
 app.use(express.json());
 
 
+const db = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+  });
 
+  db.connect((err) => {
+    if (err) {
+        console.err("Error to connect in Database, err")
+    } else {
+        console.log("MySQL database connected :)")
+    }
+  })
 
 
 
 
 app.listen(port, () => {
-    console.log(`app is listening at port ${port}`);
+    console.log(`app listening at port ${port}`);
 });
